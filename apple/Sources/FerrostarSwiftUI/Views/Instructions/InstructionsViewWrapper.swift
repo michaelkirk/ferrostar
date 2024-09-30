10666662ac0cc5cb65a5dee418831c9dadf2ca22
Michael Kirk <michael.code@endoftheworl.de>
2024-09-30 12:32:18 -0700

    Work around missing instruction banner w/ travelmux

--- apple/Sources/FerrostarSwiftUI/Views/Instructions/InstructionsViewWrapper.swift
+++ apple/Sources/FerrostarSwiftUI/Views/Instructions/InstructionsViewWrapper.swift
@@ -11,9 +11,13 @@ struct InstructionsViewWrapper: View {
 
     var body: some View {
         if case .navigating = navigationState?.tripState,
-           let visualInstruction = navigationState?.currentVisualInstruction,
            let progress = navigationState?.currentProgress,
-           let remainingSteps = navigationState?.remainingSteps
+           let remainingSteps = navigationState?.remainingSteps,
+           // REVIEW: is this still necesarry?
+           // FIXME: without this long stretches of the trip show no banner.
+           // This seems to be a difference with travelmux vs. stadia
+           let visualInstruction = (navigationState?.currentVisualInstruction ?? remainingSteps
+               .compactMap(\.visualInstructions.first).first)
         {
             InstructionsView(
                 visualInstruction: visualInstruction,
