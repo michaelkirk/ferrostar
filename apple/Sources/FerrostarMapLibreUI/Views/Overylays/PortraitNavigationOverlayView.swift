f6d30b572c54b94dc4395e1a5a8d5a6d5a29741e
Michael Kirk <michael.code@endoftheworl.de>
2024-09-30 12:32:18 -0700

    Work around missing instruction banner w/ travelmux

--- apple/Sources/FerrostarMapLibreUI/Views/Overylays/PortraitNavigationOverlayView.swift
+++ apple/Sources/FerrostarMapLibreUI/Views/Overylays/PortraitNavigationOverlayView.swift
@@ -122,9 +122,12 @@ struct PortraitNavigationOverlayView<T: SpokenInstructionObserver & ObservableOb
             .padding(.top, instructionsViewSizeWhenNotExpanded.height + 16)
 
             if case .navigating = navigationState?.tripState,
-               let visualInstruction = navigationState?.currentVisualInstruction,
                let progress = navigationState?.currentProgress,
-               let remainingSteps = navigationState?.remainingSteps
+               let remainingSteps = navigationState?.remainingSteps,
+               // FIXME: without this long stretches of the trip show no banner.
+               // This seems to be a difference with travelmux vs. stadia
+               let visualInstruction = (navigationState?.currentVisualInstruction ?? remainingSteps
+                   .compactMap(\.visualInstructions.first).first)
             {
                 InstructionsView(
                     visualInstruction: visualInstruction,
