6288f85cdbb7d3312c43311f0c54f392f8c70957
Michael Kirk <michael.code@endoftheworl.de>
2025-05-14 21:24:21 -0600

    Show 'progress' view when trip complete (TripCompleteBanner)
    For some reason this isn't necessary with the landscape view, which
    always shows it's progress view.

--- apple/Sources/FerrostarMapLibreUI/Views/Overylays/PortraitNavigationOverlayView.swift
+++ apple/Sources/FerrostarMapLibreUI/Views/Overylays/PortraitNavigationOverlayView.swift
@@ -106,10 +106,11 @@ struct PortraitNavigationOverlayView: View, CustomizableNavigatingInnerGridView 
                         if case .hidden = cameraControlState {
                             currentRoadNameView(navigationState)
                         }
-
-                        progressView(navigationState, onTapExit)
                     }
                 }
+                // Maps.Earth: We also show a `trip complete` state in our progress view, so
+                // we want to render it even when not case .navigating
+                progressView(navigationState, onTapExit)
             }
             .padding(.top, instructionsViewSizeWhenNotExpanded.height + 16)
 
